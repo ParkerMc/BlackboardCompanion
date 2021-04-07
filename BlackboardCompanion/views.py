b9ce80d6752947bf8931b49426fd17eae279e156
caige13 <34533619+caige13@users.noreply.github.com>
2021-04-07 00:27:00 -0500

    Update Views.py
    fixed bug inside register

--- BlackboardCompanion/views.py
+++ BlackboardCompanion/views.py
@@ -27,7 +27,7 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect('/home')
+            return redirect('/home')
     else:
         form = UserCreationForm()
 
