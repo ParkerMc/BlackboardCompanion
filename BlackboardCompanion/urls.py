e82ba46de3523e74601ccfd9cb235d3dc7a4fdac
caige13 <34533619+caige13@users.noreply.github.com>
2021-04-09 20:07:10 -0500

    Login function
    Added Login Functionality

--- BlackboardCompanion/urls.py
+++ BlackboardCompanion/urls.py
@@ -21,10 +21,10 @@ from BlackboardCompanion import views, settings
 
 
 urlpatterns = [
-    path('', views.blank),
-    path('home/', views.home),
+    path('', views.blank_view),
+    path('home/', views.home_view),
     path('admin/', admin.site.urls),
-    path('login/', views.login),
-    path('register/', views.register)
+    path('login/', views.login_view),
+    path('register/', views.register_view)
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- BlackboardCompanion/views.py
+++ BlackboardCompanion/views.py
@@ -1,26 +1,40 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 
 # /
-def blank(request):
+def blank_view(request):
     return redirect("/home")
 
-def home(request):
+def home_view(request):
     template = loader.get_template('home.html')
     context = {}
     return HttpResponse(template.render(context, request))
 
 
 # /login
-def login(request):
+def login_view(request):
     template = loader.get_template('login.html')
+
+    if request.user.is_authenticated:
+        logout(request)
+
+    if request.method == "POST":
+        netID = request.POST['netid']
+        print(netID)
+        password = request.POST['password']
+        user = authenticate(request, username=netID, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/home")
+        #else: For displaying error message
+
     context = {}
     return HttpResponse(template.render(context, request))
 
-def register(request):
+def register_view(request):
     template = loader.get_template('register.html')
 
     if request.method == "POST":
