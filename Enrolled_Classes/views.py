5c58f1201992cc49259cd5745687e46df61b2eab
Phuoc Pham <phuocdt17@gmail.com>
2021-04-11 15:07:18 -0500

    update add class

--- Enrolled_Classes/views.py
+++ Enrolled_Classes/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def enrolledClasses_view(request):
@@ -10,8 +10,15 @@ def enrolledClasses_view(request):
 
     return HttpResponse(template.render(context, request))
 
+@login_required(login_url='/login/')
 def addClass_view(request):
     template = loader.get_template('enrolled_classes/addClass.html')
     context = {}
+    if request.method == "POST":
+       coursename=request.POST["CourseName"]
+       sectionnum=request.POST["Section"]
+       codecourse=request.POST["Course"]
+       print(coursename)
+       return redirect("/enrolled-classes")
 
     return HttpResponse(template.render(context, request))
