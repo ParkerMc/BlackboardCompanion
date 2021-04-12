be5e26068d160a72eae657e9f080f7b0e1810c91
ParkerMc <parkermc@parkermc.net>
2021-04-12 08:45:17 -0500

    Fix linux setup shell scripts

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip3 install -r requirements.txt
-python manage.py migrate --settings=BlackboardCompanion.settings.prod
+python3 manage.py migrate --settings=BlackboardCompanion.settings.prod
--- setup_dev.sh
+++ setup_dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip3 install -r requirements.txt
-python manage.py migrate --settings=BlackboardCompanion.settings.dev
+python3 manage.py migrate --settings=BlackboardCompanion.settings.dev
