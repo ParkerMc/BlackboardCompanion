e43f1b45b360c2e65df251a28d5547b86fced9a6
ParkerMc <parkermc@parkermc.net>
2021-04-13 21:24:34 -0500

    Update static root for real

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 pip3 install -r requirements.txt
+python3 manage.py collectstatic --settings=BlackboardCompanion.settings.prod
 python3 manage.py migrate --settings=BlackboardCompanion.settings.prod
