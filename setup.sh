19bc4860097e55421ac024b1213702c1ebc5b4ec
ParkerMc <parkermc@parkermc.net>
2021-04-26 11:31:06 -0500

    Add --noinput to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip3 install -r requirements.txt
-python3 manage.py collectstatic --settings=BlackboardCompanion.settings.prod
+python3 manage.py collectstatic --settings=BlackboardCompanion.settings.prod --noinput
 python3 manage.py migrate --settings=BlackboardCompanion.settings.prod
